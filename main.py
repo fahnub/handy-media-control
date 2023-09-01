4115c94c3dcec36fcd82d4813f4fa1154a736557
Fahad Nadeem <itsfahadbutt@gmail.com>
2023-09-01 23:55:54 +0500

    refactored code

--- main.py
+++ main.py
@@ -1,86 +1,76 @@
-import cv2 
 import mediapipe as mp
 import pyautogui
 import time
-
-def count_fingers(lst):
-    cnt = 0
-
-    thresh = (lst.landmark[0].y*100 - lst.landmark[9].y*100)/2
-
-    if (lst.landmark[5].y*100 - lst.landmark[8].y*100) > thresh:
-        cnt += 1
-
-    if (lst.landmark[9].y*100 - lst.landmark[12].y*100) > thresh:
-        cnt += 1
-
-    if (lst.landmark[13].y*100 - lst.landmark[16].y*100) > thresh:
-        cnt += 1
-
-    if (lst.landmark[17].y*100 - lst.landmark[20].y*100) > thresh:
-        cnt += 1
-
-    if (lst.landmark[5].x*100 - lst.landmark[4].x*100) > 6:
-        cnt += 1
+import cv2 
 
 
-    return cnt 
+def count_fingers(keypoints):
+    count = 0
+    thresh = (keypoints.landmark[0].y*100 - keypoints.landmark[9].y*100)/2
+
+    if (keypoints.landmark[5].y*100 - keypoints.landmark[8].y*100) > thresh:
+        count += 1
+
+    if (keypoints.landmark[9].y*100 - keypoints.landmark[12].y*100) > thresh:
+        count += 1
+
+    if (keypoints.landmark[13].y*100 - keypoints.landmark[16].y*100) > thresh:
+        count += 1
+
+    if (keypoints.landmark[17].y*100 - keypoints.landmark[20].y*100) > thresh:
+        count += 1
+
+    if (keypoints.landmark[5].x*100 - keypoints.landmark[4].x*100) > 6:
+        count += 1
+
+    return count 
+
 
 cap = cv2.VideoCapture(0)
-
 drawing = mp.solutions.drawing_utils
 hands = mp.solutions.hands
 hand_obj = hands.Hands(max_num_hands=1)
-
-
-start_init = False 
-
+start_init = False
 prev = -1
 
 while True:
     end_time = time.time()
-    _, frm = cap.read()
-    frm = cv2.flip(frm, 1)
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    response = hand_obj.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-    res = hand_obj.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+    if response.multi_hand_landmarks:
+        hand_keypoints = response.multi_hand_landmarks[0]
+        count = count_fingers(hand_keypoints)
 
-    if res.multi_hand_landmarks:
-
-        hand_keyPoints = res.multi_hand_landmarks[0]
-
-        cnt = count_fingers(hand_keyPoints)
-
-        if not(prev==cnt):
-            if not(start_init):
+        if not prev==count:
+            if not start_init :
                 start_time = time.time()
                 start_init = True
 
-            elif (end_time-start_time) > 0.2:
-                if (cnt == 1):
+            elif end_time - start_time > 0.2:
+                if count == 1:
                     pyautogui.press("right")
                 
-                elif (cnt == 2):
+                elif count == 2:
                     pyautogui.press("left")
 
-                elif (cnt == 3):
+                elif count == 3:
                     pyautogui.press("up")
 
-                elif (cnt == 4):
+                elif count == 4:
                     pyautogui.press("down")
 
-                elif (cnt == 5):
+                elif count == 5:
                     pyautogui.press("space")
 
-                prev = cnt
+                prev = count
                 start_init = False
 
+        # drawing.draw_landmarks(frame, hand_keypoints, hands.HAND_CONNECTIONS)
 
-        
-
-
-        drawing.draw_landmarks(frm, hand_keyPoints, hands.HAND_CONNECTIONS)
-
-    cv2.imshow("window", frm)
+    frame = cv2.resize(frame, (320, 240)) 
+    cv2.imshow("window", frame)
 
     if cv2.waitKey(1) == 27:
         cv2.destroyAllWindows()
